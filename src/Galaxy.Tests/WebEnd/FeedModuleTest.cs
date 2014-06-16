293dd7134cea34a1ce68d6031db0b95a21a73741
Jury Soldatenkov <yurysold@googlemail.com>
2014-06-16 23:36:30 +0400

    Added test stub for feed module

---
+++ src/Galaxy.Tests/WebEnd/FeedModuleTest.cs
@@ -0,0 +1,37 @@
+﻿using Nancy;
+using Nancy.Testing;
+using NUnit.Framework;
+
+namespace Codestellation.Galaxy.Tests.WebEnd
+{
+    [TestFixture]
+    public class FeedModuleTest
+    {
+        private Bootstrapper _bootstrapper;
+        private Browser _browser;
+
+        [SetUp]
+        public void Create_browser_and_log_in()
+        {
+            _bootstrapper = new Bootstrapper();
+            _browser = new Browser(_bootstrapper);
+
+            var loginResponse = _browser.Post("/login", with =>
+            {
+
+                with.HttpRequest();
+                with.FormValue("Login", "admin");
+                with.FormValue("Password", "admin");
+            });
+        }
+
+
+        [Test]
+        public void Shows_all_feeds_from_dashboard()
+        {
+            var response = _browser.Get("/feed", with => with.HttpRequest());
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
