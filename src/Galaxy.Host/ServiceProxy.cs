b375b00f43b673275cd48c3165cc4fb18173362e
Jury Soldatenkov <yurysold@googlemail.com>
2014-06-23 23:42:21 +0400

    Implemented stronger checks in service proxy

--- src/Galaxy.Host/ServiceProxy.cs
+++ src/Galaxy.Host/ServiceProxy.cs
@@ -1,37 +1,53 @@
 using System;
 using System.Reflection;
+using NLog;
 
 namespace Codestellation.Galaxy.Host
 {
     internal class ServiceProxy
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         private readonly MethodInfo _startMethod;
         private readonly MethodInfo _stopMethod;
         private readonly object _instance;
-        private readonly bool _initialized;
 
         public ServiceProxy(ServiceConfig config)
         {
             var serviceType = Type.GetType(config.AssemblyQualifiedType);
 
-            if (_initialized = (serviceType != null))
+            if (serviceType == null)
             {
-                _instance = Activator.CreateInstance(serviceType);
-                _startMethod = serviceType.GetMethod(config.StartMethod);
-                _stopMethod = serviceType.GetMethod(config.StopMethod);
+                var message = string.Format("Type '{0}' not found", config.AssemblyQualifiedType);
+                Logger.Fatal(message);
+                throw new InvalidOperationException(message);
             }
+
+            _instance = Activator.CreateInstance(serviceType);
+            _startMethod = GetMethodOrThrow(config.StartMethod, serviceType);
+            _stopMethod = GetMethodOrThrow(config.StopMethod, serviceType);
+        }
+
+        private static MethodInfo GetMethodOrThrow(string methodName, Type serviceType)
+        {
+            var result = serviceType.GetMethod(methodName);
+            if (result != null)
+            {
+                return result;
+            }
+            var message = string.Format("Method '{0}' of type '{1}' not found", methodName, serviceType);
+            Logger.Fatal(message);
+            throw new InvalidOperationException(message);
         }
 
         public void Start()
         {
-            if(_initialized)
-                _startMethod.Invoke(_instance, new object[0]);
+            _startMethod.Invoke(_instance, new object[0]);
         }
 
         public void Stop()
         {
-            if (_initialized)
-                _stopMethod.Invoke(_instance, new object[0]);
+            _stopMethod.Invoke(_instance, new object[0]);
         }
     }
 }
