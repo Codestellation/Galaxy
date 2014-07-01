8eee82b2af6e51c2ad822435505ab52288d54100
Andrey Zak <amzak.only@gmail.com>
2014-07-01 15:20:56 +0400

    Added test for PackageVersionCache

--- src/Galaxy.Tests/DeploymentAndOperations/OperationsTests/InstallPackageTests.cs
+++ src/Galaxy.Tests/DeploymentAndOperations/OperationsTests/InstallPackageTests.cs
@@ -1,11 +1,10 @@
-﻿using Codestellation.Galaxy.Domain;
-using Codestellation.Galaxy.ServiceManager.Operations;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
+using Codestellation.Galaxy.Domain;
+using Codestellation.Galaxy.Tests.Helpers;
+using Codestellation.Galaxy.ServiceManager.Operations;
+using NUnit.Framework;
 
 namespace Codestellation.Galaxy.Tests.DeploymentAndOperations.OperationsTests
 {
@@ -15,36 +14,13 @@ namespace Codestellation.Galaxy.Tests.DeploymentAndOperations.OperationsTests
         string output;
         string targetPath;
 
-        void ExtractEmbeddedResource(string outputDir, string resourceLocation, IEnumerable<string> files)
-        {
-            foreach (string file in files)
-            {
-                using (System.IO.Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceLocation + @"." + file))
-                {
-                    using (System.IO.FileStream fileStream = new System.IO.FileStream(System.IO.Path.Combine(outputDir, file), System.IO.FileMode.Create))
-                    {
-                        stream.CopyTo(fileStream);
-                        stream.Flush();
-                        fileStream.Close();
-                    }
-                }
-            }
-        }
-
         [SetUp]
         public void Init()
         {
             
             output = Path.Combine(Environment.CurrentDirectory, "testnuget");
 
-            if(!Directory.Exists(output))
-                Directory.CreateDirectory(output);
-
-            ExtractEmbeddedResource(output, "Codestellation.Galaxy.Tests.Resources", new string[] { "TestNugetPackage.1.0.0" });
-
-            File.Move(
-                Path.Combine(output, "TestNugetPackage.1.0.0"),
-                Path.Combine(output, "TestNugetPackage.1.0.0.nupkg"));
+            ResourcesHelper.ExtractEmbeddedAndRename(output, "Codestellation.Galaxy.Tests.Resources", "TestNugetPackage.1.0.0", "TestNugetPackage.1.0.0.nupkg");
 
             targetPath = Path.Combine(output, "extracted");
 
@@ -55,7 +31,12 @@ namespace Codestellation.Galaxy.Tests.DeploymentAndOperations.OperationsTests
         {
             InstallPackage op = new InstallPackage(
                 targetPath,
-                new Deployment() { DisplayName = "testdeployment"},
+                new Deployment() 
+                { 
+                    DisplayName = "testdeployment",
+                    PackageName = "TestNugetPackage",
+                    PackageVersion = new Version(1,0)
+                },
                 new NugetFeed()
                 {
                     Name = "TestNugetPackage",
@@ -69,6 +50,27 @@ namespace Codestellation.Galaxy.Tests.DeploymentAndOperations.OperationsTests
             var expectedDll = dllFiles.FirstOrDefault(item => item.Contains("TestNugetPackLib.dll"));
 
             Assert.That(expectedDll, Is.Not.Null);
+        }
+
+        [Test]
+        public void InstallPackage_extract_package_no_version_fail()
+        {
+            InstallPackage op = new InstallPackage(
+                targetPath,
+                new Deployment()
+                {
+                    DisplayName = "testdeployment",
+                    PackageName = "TestNugetPackage"
+                },
+                new NugetFeed()
+                {
+                    Name = "TestNugetPackage",
+                    Uri = output
+                });
+
+
+
+            Assert.That(() => op.Execute(),Throws.TypeOf<ArgumentException>() );
         }
 
         [TearDown]
---
+++ src/Galaxy.Tests/InfrastructureTests/VersionPackageCacheTests.cs
@@ -0,0 +1,59 @@
+﻿using Codestellation.Galaxy.Infrastructure;
+using Codestellation.Galaxy.Tests.Helpers;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace Codestellation.Galaxy.Tests.InfrastructureTests
+{
+    [TestFixture]
+    public class VersionPackageCacheTests
+    {
+        const string testPackageName = "TestNugetPackage";
+
+        string output;
+
+        [SetUp]
+        public void Init()
+        {
+            output = Path.Combine(Environment.CurrentDirectory, "testnuget");
+
+            ResourcesHelper.ExtractEmbeddedAndRename(output, "Codestellation.Galaxy.Tests.Resources", "TestNugetPackage.1.0.0", "TestNugetPackage.1.0.0.nupkg");
+            ResourcesHelper.ExtractEmbeddedAndRename(output, "Codestellation.Galaxy.Tests.Resources", "TestNugetPackage.1.1.0", "TestNugetPackage.1.1.0.nupkg");
+        }
+
+        [Test]
+        public void VersionPackageCache_retrieve_package_versions_success()
+        {
+            ManualResetEventSlim refreshCompleted = new ManualResetEventSlim(false);
+
+            VersionPackageCache versionPackageCache = new VersionPackageCache();
+            versionPackageCache.OnCacheUpdated += new EventHandler((sender, e) => refreshCompleted.Set());
+
+            versionPackageCache.AddPackage(testPackageName, new Domain.NugetFeed() 
+            { 
+                Name = "test_feed",
+                Uri = output
+            });
+
+            refreshCompleted.Wait();
+
+            var packageVersions = versionPackageCache.GetPackageVersions(testPackageName);
+
+            Version[] sampleVesrions = new Version[]
+            {
+                new Version(1,1,0,0),
+                new Version(1,0,0,0)
+            };
+
+            Assert.That(packageVersions, Is.EquivalentTo(sampleVesrions));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            Directory.Delete(output, true);
+        }
+    }
+}
