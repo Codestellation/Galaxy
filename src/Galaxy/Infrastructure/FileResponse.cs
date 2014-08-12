47a7f5b72c73ea30547048bf6474963097a598f2
Jury Soldatenkov <yurysold@gmail.com>
2014-08-12 18:21:48 +0400

    Close file stream on when file response completed

--- src/Galaxy/Infrastructure/FileResponse.cs
+++ src/Galaxy/Infrastructure/FileResponse.cs
@@ -5,12 +5,22 @@ namespace Codestellation.Galaxy.Infrastructure
 {
     public class FileResponse : Response
     {
+        private string _fullPath;
+
         public FileResponse(string fullPath)
         {
-            ContentType = MimeTypes.GetMimeType(fullPath);
-            var content = File.OpenRead(fullPath);
+            _fullPath = fullPath;
 
-            Contents = content.CopyTo;
+            ContentType = MimeTypes.GetMimeType(fullPath);
+            Contents = CopyFromFile;
+        }
+
+        private void CopyFromFile(Stream responseStream)
+        {
+            using (var content = File.OpenRead(_fullPath))
+            {
+                content.CopyTo(responseStream);
+            }
         }
     }
 }
