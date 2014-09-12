51d67369a511b93160218e612f4388dcd22fc505
Egor Gorbachev <egorbachev@corp.finam.ru>
2014-09-12 18:00:28 +0400

    Closed #17 Extracted operation creation to OperationBuilder

---
+++ src/Galaxy/ServiceManager/OperationBuilder.cs
@@ -0,0 +1,82 @@
+using Codestellation.Galaxy.Domain;
+using Codestellation.Galaxy.ServiceManager.Operations;
+
+namespace Codestellation.Galaxy.ServiceManager
+{
+    public class OperationBuilder
+    {
+        private readonly Options _options;
+
+        public OperationBuilder(Options options)
+        {
+            _options = options;
+        }
+        
+        public IOperation BackupService(Deployment deployment)
+        {
+            var serviceName = deployment.GetServiceName();
+            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
+            var backupFolder = deployment.GetBackupFolder();
+            return new BackupService(serviceName, serviceFolder, backupFolder);
+        }
+
+        public IOperation ClearBinaries(Deployment deployment)
+        {
+            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
+            var keepOnUpdate = deployment.KeepOnUpdate.Clone();
+            return new ClearBinaries(serviceFolder, keepOnUpdate);
+        }
+
+        public IOperation InstallPackage(Deployment deployment, NugetFeed deploymentFeed, FileList keepOnUpdate)
+        {
+            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
+            var packageId = deployment.PackageId;
+            var feedUri = deploymentFeed.Uri;
+            var packageVersion = deployment.PackageVersion;
+            var orders = new[] { new InstallPackageOrder(packageId, feedUri, packageVersion) };
+            return new InstallPackage(serviceFolder, orders, keepOnUpdate);
+        }
+
+        public IOperation UninstallPackage(Deployment deployment)
+        {
+            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
+            return new UninstallPackage(serviceFolder);
+        }
+
+        public IOperation StartService(Deployment deployment)
+        {
+            var serviceName = deployment.GetServiceName();
+            return new StartService(serviceName);
+        }
+
+        public IOperation StopService(Deployment deployment)
+        {
+            var serviceName = deployment.GetServiceName();
+            return new StopService(serviceName);
+        }
+
+        public IOperation InstallService(Deployment deployment)
+        {
+            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
+            var hostFileName = deployment.GetServiceHostFileName();
+            var instanceName = deployment.InstanceName;
+            return new InstallService(serviceFolder, hostFileName, instanceName);
+        }
+
+        public IOperation UninstallService(Deployment deployment)
+        {
+            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
+            var hostFileName = deployment.GetServiceHostFileName();
+            var instanceName = deployment.InstanceName;
+            return new UninstallService(serviceFolder, hostFileName, instanceName);
+        }
+
+        public IOperation OverrideFiles(Deployment deployment)
+        {
+            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
+            var serviceFilesFolder = deployment.GetFilesFolder();
+            var keepOnUpdate = deployment.KeepOnUpdate.Clone();
+            return new OverrideFiles(serviceFolder, serviceFilesFolder, keepOnUpdate);
+        }
+    }
+}
--- src/Galaxy/ServiceManager/TaskBuilder.cs
+++ src/Galaxy/ServiceManager/TaskBuilder.cs
@@ -1,62 +1,61 @@
 ﻿using System;
 using System.IO;
 using Codestellation.Galaxy.Domain;
-using Codestellation.Galaxy.ServiceManager.Operations;
 using Codestellation.Quarks.IO;
 
 namespace Codestellation.Galaxy.ServiceManager
 {
     public class TaskBuilder
     {
-        private readonly Options _options;
+        private readonly OperationBuilder _operations;
 
-        public TaskBuilder(Options options)
+        public TaskBuilder(OperationBuilder operationBuilder)
         {
-            _options = options;
+            _operations = operationBuilder;
         }
 
         public DeploymentTask DeployServiceTask(Deployment deployment, NugetFeed deploymentFeed)
         {
             return CreateDeployTask("DeployService", deployment)
-                .Add(BackupService(deployment))
-                .Add(InstallPackage(deployment, deploymentFeed, FileList.Empty))
-                .Add(OverrideFiles(deployment));
+                .Add(_operations.BackupService(deployment))
+                .Add(_operations.InstallPackage(deployment, deploymentFeed, FileList.Empty))
+                .Add(_operations.OverrideFiles(deployment));
         }
 
         public DeploymentTask UpdateServiceTask(Deployment deployment, NugetFeed deploymentFeed)
         {
             return CreateDeployTask("UpdateService", deployment)
-                .Add(StopService(deployment))
-                .Add(BackupService(deployment))
-                .Add(ClearBinaries(deployment))
-                .Add(InstallPackage(deployment, deploymentFeed, deployment.KeepOnUpdate.Clone()))
-                .Add(OverrideFiles(deployment));
+                .Add(_operations.StopService(deployment))
+                .Add(_operations.BackupService(deployment))
+                .Add(_operations.ClearBinaries(deployment))
+                .Add(_operations.InstallPackage(deployment, deploymentFeed, deployment.KeepOnUpdate.Clone()))
+                .Add(_operations.OverrideFiles(deployment));
         }
 
         public DeploymentTask InstallServiceTask(Deployment deployment, NugetFeed deploymentFeed)
         {
             return CreateDeployTask("InstallService", deployment)
-                .Add(InstallService(deployment));
+                .Add(_operations.InstallService(deployment));
         }
 
         public DeploymentTask UninstallServiceTask(Deployment deployment, NugetFeed deploymentFeed)
         {
             return CreateDeployTask("UninstallService", deployment)
-                .Add(StopService(deployment))
-                .Add(UninstallService(deployment))
-                .Add(UninstallPackage(deployment));
+                .Add(_operations.StopService(deployment))
+                .Add(_operations.UninstallService(deployment))
+                .Add(_operations.UninstallPackage(deployment));
         }
 
         public DeploymentTask StartServiceTask(Deployment deployment, NugetFeed deploymentFeed)
         {
             return CreateDeployTask("StartService", deployment)
-                .Add(StartService(deployment));
+                .Add(_operations.StartService(deployment));
         }
 
         public DeploymentTask StopServiceTask(Deployment deployment, NugetFeed deploymentFeed)
         {
             return CreateDeployTask("StopService", deployment)
-                .Add(StopService(deployment));
+                .Add(_operations.StopService(deployment));
         }
 
         private static DeploymentTask CreateDeployTask(string name, Deployment deployment)
@@ -69,73 +68,6 @@ namespace Codestellation.Galaxy.ServiceManager
             var logStream = File.Open(fullPath, FileMode.Create, FileAccess.Write);
             
             return new DeploymentTask(name, deployment.Id, logStream);
-        }
-
-        private IOperation BackupService(Deployment deployment)
-        {
-            var serviceName = deployment.GetServiceName();
-            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
-            var backupFolder = deployment.GetBackupFolder();
-            return new BackupService(serviceName, serviceFolder, backupFolder);
-        }
-
-        private IOperation ClearBinaries(Deployment deployment)
-        {
-            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
-            var keepOnUpdate = deployment.KeepOnUpdate.Clone();
-            return new ClearBinaries(serviceFolder, keepOnUpdate);
-        }
-
-        private IOperation InstallPackage(Deployment deployment, NugetFeed deploymentFeed, FileList keepOnUpdate)
-        {
-            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
-            var packageId = deployment.PackageId;
-            var feedUri = deploymentFeed.Uri;
-            var packageVersion = deployment.PackageVersion;
-            var orders = new[] { new InstallPackageOrder(packageId, feedUri, packageVersion) };
-            return new InstallPackage(serviceFolder, orders, keepOnUpdate);
-        }
-
-        private IOperation UninstallPackage(Deployment deployment)
-        {
-            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
-            return new UninstallPackage(serviceFolder);
-        }
-
-        private IOperation StartService(Deployment deployment)
-        {
-            var serviceName = deployment.GetServiceName();
-            return new StartService(serviceName);
-        }
-
-        private IOperation StopService(Deployment deployment)
-        {
-            var serviceName = deployment.GetServiceName();
-            return new StopService(serviceName);
-        }
-
-        private IOperation InstallService(Deployment deployment)
-        {
-            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
-            var hostFileName = deployment.GetServiceHostFileName();
-            var instanceName = deployment.InstanceName;
-            return new InstallService(serviceFolder, hostFileName, instanceName);
-        }
-
-        private IOperation UninstallService(Deployment deployment)
-        {
-            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
-            var hostFileName = deployment.GetServiceHostFileName();
-            var instanceName = deployment.InstanceName;
-            return new UninstallService(serviceFolder, hostFileName, instanceName);
-        }
-
-        private IOperation OverrideFiles(Deployment deployment)
-        {
-            var serviceFolder = deployment.GetDeployFolder(_options.GetDeployFolder());
-            var serviceFilesFolder = deployment.GetFilesFolder();
-            var keepOnUpdate = deployment.KeepOnUpdate.Clone();
-            return new OverrideFiles(serviceFolder, serviceFilesFolder, keepOnUpdate);
         }
     }
 }
