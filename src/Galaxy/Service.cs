4d03e84187d7705c5de651514fe7e0a12ebacdc3
Jury Soldatenkov <yurysold@gmail.com>
2014-09-22 22:13:01 +0400

    Nancy self host replaced with owin and katana

--- src/Galaxy/Service.cs
+++ src/Galaxy/Service.cs
@@ -1,29 +1,18 @@
 ﻿using System;
 using System.Configuration;
-using Nancy.Hosting.Self;
-using NLog;
+using Microsoft.Owin.Hosting;
 
 namespace Codestellation.Galaxy
 {
     public class Service
     {
-        private readonly NancyHost _nancyHost;
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private IDisposable _owinHost;
+        private string _uriString;
 
         public Service()
         {
             var port = Convert.ToInt32(ConfigurationManager.AppSettings["port"]);
-            var uriString = string.Format("http://localhost:{0}", port);
-            var hostConfiguration = new HostConfiguration
-                                    {
-                                        UnhandledExceptionCallback = OnUnhandledException
-                                    };
-            _nancyHost = new NancyHost(new Uri(uriString), new Bootstrapper(), hostConfiguration);
-        }
-
-        private static void OnUnhandledException(Exception ex)
-        {
-            Logger.Error("Nancy got unhandled exception", ex);
+            _uriString = string.Format("http://localhost:{0}", port);
         }
 
         public void Start()
@@ -38,14 +27,16 @@ namespace Codestellation.Galaxy
             //netsh http add urlacl url=http://+:80/ user=Все
 
             //more information http://msdn.microsoft.com/en-us/library/ms733768.aspx and https://github.com/NancyFx/Nancy/wiki/Self-Hosting-Nancy
-            
-            _nancyHost.Start();
+
+            _owinHost = WebApp.Start<OwinStartup>(_uriString);
         }
 
         public void Stop()
         {
-            _nancyHost.Stop();
-            _nancyHost.Dispose();
+            if (_owinHost != null)
+            {
+                _owinHost.Dispose();
+            }
         }
     }
 }
---
+++ src/Galaxy/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Owin;
+
+namespace Codestellation.Galaxy
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // TODO: May throw such exception (at least on my windows 8.1 pro). Investigation needed.
+            // The Nancy self host was unable to start, as no namespace reservation existed for the provided url(s).
+            // Please either enable UrlReservations.CreateAutomatically on the HostConfiguration provided to 
+            // the NancyHost, or create the reservations manually with the (elevated) command(s):
+            // netsh http add urlacl url=http://+/ user=Everyone
+            // On russian machines this command could look like this:
+            // netsh http add urlacl url=http://+:80/ user=Все
+            app.UseNancy();
+        }
+    }
+}
