09f43a7c1a5dfa7286c5e0c1a3630c059b5e0d63
Jury Soldatenkov <yurysold@gmail.com>
2014-10-16 18:37:44 +0400

    Added clean-up operation to remove logs #21

---
+++ src/Galaxy.Tests/Maintenance/CleanUpLogTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using Codestellation.Galaxy.Maintenance;
+using Codestellation.Galaxy.ServiceManager.Operations;
+using Codestellation.Quarks.IO;
+using NUnit.Framework;
+
+namespace Codestellation.Galaxy.Tests.Maintenance
+{
+    [TestFixture]
+    public class CleanUpLogTests
+    {
+        private string _folder;
+        private string _oldLog;
+        private string _newLog;
+
+        [SetUp]
+        public void Setup()
+        {
+            _folder = Folder.ToFullPath("Test");
+            Folder.EnsureExists(_folder);
+
+            _oldLog = Folder.Combine(_folder, "old.log");
+            _newLog = Folder.Combine(_folder, "new.log");
+
+            File.WriteAllText(_oldLog, "The data");
+            File.WriteAllText(_newLog, "The data");
+
+            File.SetLastWriteTime(_oldLog, DateTime.Now.AddDays(-20));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Folder.EnsureDeleted(_folder);
+        }
+
+        [Test]
+        public void Will_remove_old_files_from_folder()
+        {
+            int timeToLive = 10;
+
+            PerformCleanup(timeToLive);
+
+            Assert.That(File.Exists(_oldLog), Is.False, "Old log was not deleted");
+            Assert.That(File.Exists(_newLog), Is.True, "New log was deleted");
+        }
+
+        [Test]
+        public void Will_not_remove_old_files_if_time_to_live_is_zero()
+        {
+            int timeToLive = 0;
+
+            PerformCleanup(timeToLive);
+
+            Assert.That(File.Exists(_oldLog), Is.True, "Old log was deleted");
+            Assert.That(File.Exists(_newLog), Is.True, "New log was deleted");
+        }
+
+        private void PerformCleanup(int timeToLive)
+        {
+            var cleanUp = new CleanUpLog();
+
+            var stringWriter = new StringWriter();
+            var context = new DeploymentTaskContext(stringWriter);
+            context.SetValue(CleanUpLog.LogFolders, new[] {_folder});
+
+            context.SetValue(CleanUpLog.PurgeLogOlderThen, timeToLive);
+
+            cleanUp.Execute(context);
+
+            Console.WriteLine(stringWriter.ToString());
+        }
+    }
+}
---
+++ src/Galaxy/Maintenance/CleanUpLog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Codestellation.Galaxy.ServiceManager.Operations;
+using Codestellation.Quarks.DateAndTime;
+using Codestellation.Quarks.IO;
+
+namespace Codestellation.Galaxy.Maintenance
+{
+    public class CleanUpLog : IOperation
+    {
+        public const string LogFolders = "Maintenance.LogFolders";
+        public const string PurgeLogOlderThen = "Maintenance.PurgeLogOlderThen";
+
+        public void Execute(DeploymentTaskContext context)
+        {
+            var folders = context.GetValue<string[]>(LogFolders);
+            var timeToLive = context.GetValue<int>(PurgeLogOlderThen);
+
+            if (timeToLive <= 0)
+            {
+                context.BuildLog.WriteLine("Purge Logs Older than is set to {0}. Skipped.", timeToLive);
+                return;
+            }
+
+            var lastDateToLive = Clock.UtcNow.AddDays(-timeToLive).Date;
+
+            foreach (var folder in folders)
+            {
+                RemoveOldFiles(folder, lastDateToLive, context.BuildLog);
+            }
+        }
+
+        private void RemoveOldFiles(string folder, DateTime lastDateToLive, TextWriter buildLog)
+        {
+            var files = Folder.EnumerateFiles(folder);
+
+            foreach (var file in files.Where(f => f.LastWriteTime < lastDateToLive))
+            {
+                buildLog.WriteLine("File {0} last write {1}. Removed", file.FullName, file.LastWriteTime.ToString(CultureInfo.InvariantCulture));
+                file.Delete();
+            }
+        }
+    }
+}
