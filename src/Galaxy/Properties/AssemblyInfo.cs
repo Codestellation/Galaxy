e164d3f51fd0e6ecb7f1756bfd431c2c751ef47e
Jury Soldatenkov <yurysold@gmail.com>
2014-10-21 18:32:15 +0400

    Show galaxy version

---
+++ src/Galaxy/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Codestellation.Galaxy")]
+[assembly: AssemblyDescription("Manage windows services")]
+[assembly: AssemblyCompany("Codestellation project")]
+[assembly: AssemblyProduct("Codestellation Galaxy")]
+[assembly: AssemblyCopyright("Copyright Codestellation project © 2014")]
+[assembly: AssemblyTrademark("Codestellation")]
