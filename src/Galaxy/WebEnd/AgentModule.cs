c3c56144f16ea3a7fb069ded526902ee2837aa63
Jury Soldatenkov <yurysold@googlemail.com>
2014-11-06 00:17:41 +0300

    Added placeholder for agents view

--- src/Galaxy/WebEnd/AgentModule.cs
+++ src/Galaxy/WebEnd/AgentModule.cs
@@ -1,7 +1,27 @@
-﻿namespace Codestellation.Galaxy.WebEnd
+﻿using Codestellation.Galaxy.Domain;
+using Codestellation.Galaxy.WebEnd.Models;
+
+namespace Codestellation.Galaxy.WebEnd
 {
-    public class AgentModule : ModuleBase
+    public class AgentModule : CrudModule
     {
+        private readonly DashBoard _dashBoard;
         public const string Path = "agent";
+
+        public AgentModule(DashBoard dashBoard) : base(Path)
+        {
+            _dashBoard = dashBoard;
+        }
+
+
+        protected override CrudOperations SupportedOperations
+        {
+            get { return CrudOperations.GetList; }
+        }
+
+        protected override object GetList(dynamic parameters)
+        {
+            return new AgentListModel();
+        }
     }
 }
---
+++ src/Galaxy/WebEnd/Models/AgentListModel.cs
@@ -0,0 +1,26 @@
+﻿using Nejdb.Bson;
+
+namespace Codestellation.Galaxy.WebEnd.Models
+{
+    public class AgentListModel
+    {
+        private AgentItemModel[] _agents;
+
+        public AgentItemModel[] Agents
+        {
+            get { return _agents ?? new AgentItemModel[0]; }
+            set { _agents = value; }
+        }
+    }
+
+    public class AgentItemModel
+    {
+        public ObjectId Id { get; set; }
+        
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public bool Connected { get; set; }
+    }
+}
