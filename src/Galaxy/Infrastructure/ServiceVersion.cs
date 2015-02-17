a7b078d755b945fe94789915667fe9e7bbd80ae9
Jury Soldatenkov <yurysold@googlemail.com>
2015-02-18 00:17:32 +0300

    Fixed error with non-public assembly version class

---
+++ src/Galaxy/Infrastructure/ServiceVersion.cs
@@ -0,0 +1,12 @@
+﻿using Codestellation.Quarks.Reflection;
+
+namespace Codestellation.Galaxy.Infrastructure
+{
+    public static class ServiceVersion
+    {
+        public static string InformationalVersion
+        {
+            get { return AssemblyVersion.InformationalVersion; }
+        }
+    }
+}
