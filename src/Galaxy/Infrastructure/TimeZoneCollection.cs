4543687e693f5dace68a30297e44b464316a9c04
Jury Soldatenkov <yurysold@googlemail.com>
2015-02-19 22:43:16 +0300

    Create job form with display name and timezone info

---
+++ src/Galaxy/Infrastructure/TimeZoneCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Codestellation.Quarks.Collections;
+
+namespace Codestellation.Galaxy.Infrastructure
+{
+    public class TimeZoneCollection
+    {
+        static TimeZoneCollection()
+        {
+            AvailableTimeZones = TimeZoneInfo
+                .GetSystemTimeZones()
+                .ConvertToArray(tzi => new KeyValuePair<string, string>(tzi.Id, tzi.DisplayName));
+        }
+
+        public static KeyValuePair<string, string>[] AvailableTimeZones { get; private set; }
+    }
+}
--- src/Galaxy/WebEnd/Models/Jobs/CreateJobModel.cs
+++ src/Galaxy/WebEnd/Models/Jobs/CreateJobModel.cs
@@ -1,10 +1,26 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Codestellation.Galaxy.WebEnd.Models.Jobs
 {
     public class CreateJobModel
     {
+        [Display(Name = "Display name", Prompt = "text")]
+        public string DisplayName { get; set; }
+
         [Display(Name = "Schedule", Prompt = "Cron Expression")]
         public string Schedule { get; set; }
+
+        private string _timeZoneId;
+
+        [Display(Name = "Time zone", Prompt = "Select timezone")]
+        public string TimeZoneId
+        {
+            get
+            {
+                return _timeZoneId ?? TimeZoneInfo.Local.Id;
+            }
+            set { _timeZoneId = value; }
+        }
     }
 }
