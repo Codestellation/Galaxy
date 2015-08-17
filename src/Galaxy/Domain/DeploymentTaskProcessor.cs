2647e8e58c71e1e4fd5b3b097de18aa1ea0cdc5f
Jury Soldatenkov <yurysold@googlemail.com>
2015-08-17 10:37:02 +0300

    Fixed deployment task processor (close #28)

--- src/Galaxy/Domain/DeploymentTaskProcessor.cs
+++ src/Galaxy/Domain/DeploymentTaskProcessor.cs
@@ -3,11 +3,14 @@ using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 using Codestellation.Galaxy.ServiceManager;
+using NLog;
 
 namespace Codestellation.Galaxy.Domain
 {
     public class DeploymentTaskProcessor : IDisposable
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         private readonly BlockingCollection<DeploymentTask> _taskQueue;
         private readonly CancellationTokenSource _cancellationSource;
         private readonly Task _processingTask;
@@ -26,16 +29,22 @@ namespace Codestellation.Galaxy.Domain
 
         private void ProcessInternal()
         {
+            DeploymentTask temp = null;
             try
             {
                 foreach (var task in _taskQueue.GetConsumingEnumerable(_cancellationSource.Token))
                 {
+                    temp = task;
                     task.Process();
                 }
             }
             catch (OperationCanceledException)
             {
                 //Operation cancelled by cancellation source
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Error processing task '{0}'", temp.Name);
             }
         }
 
