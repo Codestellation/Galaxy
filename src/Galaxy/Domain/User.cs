0916f09284a74eb594219b118ee630238d9525cc
Jury Soldatenkov <yurysold@gmail.com>
2016-01-28 23:17:50 +0300

    make users lowercase to avoid duplication

--- src/Galaxy/Domain/User.cs
+++ src/Galaxy/Domain/User.cs
@@ -4,9 +4,14 @@ namespace Codestellation.Galaxy.Domain
 {
     public class User
     {
+        private string _login;
         public ObjectId Id { get; internal set; }
 
-        public string Login { get; set; }
+        public string Login
+        {
+            get { return _login; }
+            set { _login = string.IsNullOrWhiteSpace(value) ? value : value.ToLowerInvariant(); }
+        }
 
         public bool IsAdmin { get; set; }
 
