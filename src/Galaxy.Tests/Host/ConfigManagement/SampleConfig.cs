1a245a59e6eb041ee16aa2588d6ff84a2f02842e
oakio <ilya.dubadenko@gmail.com>
2016-05-30 20:13:55 +0300

    Removed unused code related to consul (relates to f6cacf)

--- src/Galaxy.Tests/Host/ConfigManagement/SampleConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Codestellation.Galaxy.Tests.Host.ConfigManagement
-{
-    public class SampleConfig
-    {
-        public int Port { get; set; }
-
-        public string Host { get; set; }
-    }
-}
